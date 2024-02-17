fa1afd45382ccab472136bbf3525836472522542
Dadir93 <abdifatah.farah@student.moringaschool.com>
2024-02-17 18:53:57 +0300

    final

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,28 @@
 import React from "react";
 
-function Article({ title, date, preview }) {
+function Article({ title, date, preview, minutes }) {
+  const renderEmojis = (count, emoji) => {
+    return Array.from({ length: count }, (_, index) => (
+      <span key={index}>{emoji}</span>
+    ));
+  };
+
+  const renderReadingTime = minutes => {
+    if (minutes < 30) {
+      const coffeeCount = Math.ceil(minutes / 5);
+      return renderEmojis(coffeeCount, "☕️");
+    } else {
+      const bentoCount = Math.ceil(minutes / 10);
+      return renderEmojis(bentoCount, "🍱");
+    }
+  };
+
   return (
     <article>
       <h3>{title}</h3>
       <small>{date || "January 1, 1970"}</small>
       <p>{preview}</p>
+      <p>{renderReadingTime(minutes)} {minutes} min read</p>
     </article>
   );
 }
--- src/data/blog.js
+++ src/data/blog.js
@@ -1,29 +1,29 @@
-import logo from "../assets/logo";
+import logo from "../assets/dameer-removebg-preview.png";
 
 const blogData = {
-  name: "Underreacted",
+  name: "React-ion",
   image: logo,
-  about: "A blog about learning React",
+  about: "Dadir's Blog",
   posts: [
     {
       id: 1,
       title: "Components 101",
-      date: "December 15, 2020",
-      preview: "Setting up the building blocks of your site",
-      minutes: 5,
+      date: "December 15, 2023",
+      preview: "The Set-up",
+      minutes: 25,
     },
     {
       id: 2,
       title: "React Data Flow",
       date: "December 11, 2020",
-      preview: "Passing props is never passé",
-      minutes: 15,
+      preview: "Don't let Passed props slip",
+      minutes: 43,
     },
     {
       id: 3,
       title: "Function vs Class Components",
-      preview: "React, meet OOJS.",
-      minutes: 47,
+      preview: "Same yet Different",
+      minutes: 61,
     },
   ],
 };
